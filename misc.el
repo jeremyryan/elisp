be39b02878f1efe918c337443f1dbd7fd410deca
Jeremy Ryan <jeremyryan000@gmail.com>
2025-11-23 13:39:52 -0500

    Added a few functions

--- misc.el
+++ misc.el
@@ -1,5 +1,38 @@
 ;;; misc.el --- Miscellaneous utility functions -*- lexical-binding: t; -*-
 
+
+(defun org-show-elapsed-since-timestamp ()
+  "Show how much time has passed since the Org timestamp at point."
+  (interactive)
+  (let* ((ts (org-element-context))
+         (type (org-element-type ts)))
+    (unless (memq type '(timestamp))
+      (user-error "No Org timestamp at point"))
+    (let* ((start (org-element-property :raw-value ts))
+           (time (org-time-string-to-time start))
+           (now (current-time))
+           (secs (float-time (time-subtract now time)))
+           (hours (/ secs 3600))
+           (mins (/ (mod secs 3600) 60)))
+      (message "Elapsed time: %.0f hours, %.0f minutes" hours mins))))
+
+
+(defun org-show-elapsed-since-timestamp ()
+  "Show how much time has passed since the Org timestamp at point."
+  (interactive)
+  (let* ((ts (org-element-context))
+         (type (org-element-type ts)))
+    (unless (memq type '(timestamp))
+      (user-error "No Org timestamp at point"))
+    (let* ((start (org-element-property :raw-value ts))
+           (time (org-time-string-to-time start))
+           (now (current-time))
+           (secs (float-time (time-subtract now time)))
+           (hours (/ secs 3600))
+           (mins (/ (mod secs 3600) 60)))
+      (message "Elapsed time: %.0f hours, %.0f minutes" hours mins))))
+
+
 (defun insert-filenames-from-directory (dir)
   "Insert the names of all non-hidden files in DIR into the current buffer."
   (interactive "DDirectory: ")
